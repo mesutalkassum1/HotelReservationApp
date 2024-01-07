d071be2efcc715e9417ed0925f8b47cd8584fcc1
mesutalkassum1 <masood.alkassom@ogr.sakarya.edu.tr>
2024-01-07 16:29:24 +0300

    Update ProfileScreen.js

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,34 +1,42 @@
-import React, { useState } from 'react';
-import { View, Text, Button, StyleSheet, TouchableOpacity , TextInput} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, Button, StyleSheet, TouchableOpacity, TextInput } from 'react-native';
 import { auth, db } from "../firebase";
 import { signOut, updatePassword, signInWithEmailAndPassword, deleteUser } from 'firebase/auth';
 import { doc, query, collection, getDocs, deleteDoc, where } from 'firebase/firestore';
 import { Entypo } from '@expo/vector-icons'; // Eklediğimiz icon seti
 
 export default function ManageAccount({ navigation, setUserLoggedIn }) {
-  // const [newPassword, setNewPassword] = useState("");
   const [currentPassword, setCurrentPassword] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
-  const [userName, setUserName] = useState(""); // Eklenen kısım
+  const [userName, setUserName] = useState("");
+  const [userEmail, setUserEmail] = useState("");
+
+  const fetchUserData = async () => {
+    try {
+      const userId = auth.currentUser?.uid;
+
+      if (userId) {
+        const usersQuery = query(collection(db, 'users'), where('userId', '==', userId));
+        const usersSnapshot = await getDocs(usersQuery);
+
+        if (usersSnapshot.size > 0) {
+          const userData = usersSnapshot.docs[0].data(); // Assuming there's only one matching document
+          setUserName(userData.name);
+          setUserEmail(auth.currentUser.email);
+        } else {
+          console.error('User document does not exist.');
+        }
+      } else {
+        console.error('User ID is null or undefined.');
+      }
+    } catch (error) {
+      console.error('Error fetching user data:', error);
+    }
+  };
 
   useEffect(() => {
-    // Firestore'dan kullanıcı adını çekme
-    const fetchUserData = async () => {
-      try {
-        const userRef = doc(db, 'users', auth.currentUser.uid);
-        const userSnapshot = await getDocs(userRef);
-        if (userSnapshot.exists()) {
-          const userData = userSnapshot.data();
-          setUserName(userData.name); // Kullanıcı adını ayarla
-        }
-      } catch (error) {
-        console.error('Error fetching user data:', error);
-      }
-    };
-
     fetchUserData();
   }, []);
-
 
   const logout = async () => {
     try {
@@ -39,22 +47,6 @@ export default function ManageAccount({ navigation, setUserLoggedIn }) {
     }
   };
 
-  // const updateUserPassword = () => {
-  //   signInWithEmailAndPassword(auth, auth.currentUser.email, currentPassword)
-  //     .then((userCredential) => {
-  //       const user = userCredential.user;
-  //       updatePassword(user, newPassword).then(() => {
-  //         setNewPassword("");
-  //         setErrorMessage("");
-  //         setCurrentPassword("");
-  //       }).catch((error) => {
-  //         setErrorMessage(error.message);
-  //       });
-  //     })
-  //     .catch((error) => {
-  //       setErrorMessage(error.message);
-  //     });
-  // };
 
   const deleteUserAndReservation = async () => {
     try {
@@ -109,8 +101,13 @@ export default function ManageAccount({ navigation, setUserLoggedIn }) {
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <Text style={styles.headerText}>{userName}</Text>
-        {/* ... (eski kod devam ediyor) */}
+        <TouchableOpacity onPress={logout}>
+          <Entypo name="log-out" size={24} color="black" />
+        </TouchableOpacity>
+        <View style={styles.userInfo}>
+          <Text style={styles.headerText}>{userName}</Text>
+          <Text style={styles.emailText}>{userEmail}</Text>
+        </View>
       </View>
 
       <Text style={styles.errorText}>{errorMessage}</Text>
@@ -128,7 +125,6 @@ export default function ManageAccount({ navigation, setUserLoggedIn }) {
   );
 }
 
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -141,9 +137,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     marginBottom: 20,
   },
+  userInfo: {
+    alignItems: 'flex-end', // Değişiklik burada
+  },
   headerText: {
-    fontSize: 16,
+    fontSize: 18, // Değişiklik burada
     fontWeight: 'bold',
+    color: 'black', // Değişiklik burada
+  },
+  emailText: {
+    fontSize: 14, // Değişiklik burada
+    color: 'gray',
   },
   errorText: {
     color: 'red',
@@ -167,6 +171,24 @@ const styles = StyleSheet.create({
 
 
 
+
+
+  // const updateUserPassword = () => {
+  //   signInWithEmailAndPassword(auth, auth.currentUser.email, currentPassword)
+  //     .then((userCredential) => {
+  //       const user = userCredential.user;
+  //       updatePassword(user, newPassword).then(() => {
+  //         setNewPassword("");
+  //         setErrorMessage("");
+  //         setCurrentPassword("");
+  //       }).catch((error) => {
+  //         setErrorMessage(error.message);
+  //       });
+  //     })
+  //     .catch((error) => {
+  //       setErrorMessage(error.message);
+  //     });
+  // };
 
 
 
